e17f7de8388cdb61b527195fe3080104467bee4b
Scotchy <jules.tevissen@wanadoo.fr>
2019-02-09 07:39:37 +0100

    Add args parsing and wallet creation.

--- pwd_manager/src/main.rs
+++ pwd_manager/src/main.rs
@@ -1,25 +1,48 @@
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json; 
-
-
+extern crate crypto;
 extern crate nat_messaging;
+extern crate rand;
+extern crate base64;
+#[macro_use]
+extern crate clap;
 
 use std::io::{self, Write, Read};
 use std::fs::File;
 use std::process;
+use clap::App;
 
 mod wallet_manager;
 
-fn main() -> io::Result<()>{
+fn main() -> io::Result<()> {
+    let yaml = load_yaml!("args.yml"); 
+    let matches = App::from(yaml).get_matches();
+
     let mut wm = wallet_manager::WalletManager::new();
 
+    if let Some(wallet_name) = matches.value_of("create") {
+        if let Some(password) = matches.value_of("password") {
+            if let Some(wallet_folder) = matches.value_of("folder") {
+                print!("Creating new wallet \"{:}\" in directory \"{:}\"\n", wallet_name, wallet_folder);
+                wallet_manager::Wallet::create(wallet_folder, wallet_name, password).unwrap(); 
+            }       
+            else {
+                print!("Creating new wallet \"{:}\" in default directory \"./wallets\"\n", wallet_name);
+                wallet_manager::Wallet::create("wallets", wallet_name, password).unwrap();
+            }
+        }
+    }
+    /*
     let mut pass = String::new();
     print!("\n"); 
     std::io::stdin().read_line(&mut pass)?;
 
-    let a = wm.open_wallet(&"test2.txt".to_string(), &pass).unwrap();
-    print!("{:?}\n", a);
+    wm.open_wallet("test2.txt".to_string(), &pass);
+    //wm.add_note(0, 4, "Hey !".to_string(), "Salut !".to_string());
+    //wm.show(0);
+    let ew = wm.wallets[0].encrypt("password", "ahah".as_bytes())?; 
+    print!("{:?}", ew);*/
     Ok(())
     /*let mut i = 0;
     loop {
--- pwd_manager/src/wallet_manager.rs
+++ pwd_manager/src/wallet_manager.rs
@@ -1,36 +1,116 @@
 
 use std::fs::File;
+use std::path::Path;
+
 use std::io::{self, Read, Write};
-
-use std::collections::HashMap;
-
-use std::str::from_utf8;
-use std::clone;
-
+use rand::Rng;
+use crypto::{ symmetriccipher, buffer, aes, blockmodes };
+use crypto::buffer::{ ReadBuffer, WriteBuffer, BufferResult };
 
 #[derive(Debug)]
 #[derive(Serialize, Deserialize)]
 pub enum EncodedType {
-    Account { id : u32, name : String, password : String },
+    Account { id : u32, name : String, password : String }, //Implement interior mutability with RefCell.
     Note { id : u32, name : String, content : String }
 }
 
 #[derive(Debug)]
 #[derive(Serialize, Deserialize)]
+pub struct EncryptedWallet {
+    name : String,
+    pub encrypted_content : String
+}
+
+impl EncryptedWallet {
+
+    pub fn to_json(&self) -> Result<String, i32> {
+        let json = serde_json::to_string(&self).unwrap();
+        Ok(json)
+    }
+}
+
+
+#[derive(Debug)]
+#[derive(Serialize, Deserialize)]
 pub struct Wallet {
-    content : Vec<EncodedType>
-    
+    name : String,
+    content : Vec<EncodedType>   
 }
 
 #[allow(dead_code)]
 #[allow(unused_variables)]
 impl Wallet {
     
-    pub fn create(name  : &String, password : &String) -> Result<(), i32> {
+    pub fn check_name(name : &str) -> bool {
+        true
+    }
+
+    pub fn create(folder : &str, name  : &str, password : &str) -> Result<(), i32> {
+        
+        if !Wallet::check_name(name) {
+            return Err(0);
+        }
+        let mut path = Path::new(folder);
+        
+        if !path.is_dir() {
+            return Err(1);
+        }
+
+        let mut file = name.to_string();
+        file.push_str(".spw");
+
+        let mut path = path.to_str().unwrap().to_string();
+        path.push_str("/");
+        path.push_str(&file[..]); 
+        let path = Path::new(&path);
+        //let name = path.file_stem().unwrap().to_str().unwrap();
+
+        let mut file = File::create(path).unwrap();
+    
+        let mut json_init = "Wallet { name: \"".to_string();
+        json_init.push_str(name);
+        json_init.push_str("\", content: [] }");
+
+        let wallet = Wallet { name: name.to_string(), content : vec!() };
+        
+        file.write_all(wallet.encrypt(password, b"iv").unwrap().to_json().unwrap().as_bytes()).unwrap();
+        // Vecteur IV
         Ok(())
     }
 
-    pub fn open(path : &String, password : &String) -> Result<Wallet, i32> {
+    pub fn encrypt(&self, password : &str, iv : &[u8]) -> Result<EncryptedWallet, io::Error> {
+        let mut encryptor = aes::cbc_encryptor(
+            aes::KeySize::KeySize256, 
+            password.as_bytes(), 
+            iv, 
+            blockmodes::PkcsPadding);
+        
+        let json_wallet = self.to_json().unwrap();
+        let json_wallet = json_wallet.as_bytes();
+
+        let mut encrypted_content = Vec::<u8>::new();
+        let mut read_buffer = buffer::RefReadBuffer::new(json_wallet);
+        let mut buffer = [0; 4096];
+        let mut write_buffer = buffer::RefWriteBuffer::new(&mut buffer);
+        
+        loop {
+            let result = match encryptor.encrypt(&mut read_buffer, &mut write_buffer, true) {
+                Ok(a) => a,
+                Err(e) => return Err(io::Error::from(io::ErrorKind::BrokenPipe))
+            };
+
+            encrypted_content.extend(write_buffer.take_read_buffer().take_remaining().iter().map(|&i| i));
+
+            match result {
+                BufferResult::BufferUnderflow => break,
+                BufferResult::BufferOverflow => { }
+            }
+        }
+        let encrypted_content = base64::encode(&encrypted_content);
+        Ok(EncryptedWallet { name : self.name.clone(), encrypted_content })
+    }
+
+    pub fn open(path : String, password : &String) -> Result<Wallet, i32> {
 
         let mut f = match File::open(path) {
             Ok(file) => file,
@@ -38,12 +118,16 @@ impl Wallet {
         };
         let mut v = String::new();
         f.read_to_string(&mut v).unwrap();
-        
+
         Ok( Wallet::from_json(&v[..]) )
     }
     
+    pub fn add_note(&mut self, id : u32, name : String, content : String) {
+        self.content.push(EncodedType::Note { id, name, content });
+    }
+
     pub fn empty() -> Wallet {
-        Wallet { content : Vec::new() }
+        Wallet { name : String::new(), content : Vec::new() }
     }
 
     fn to_json(&self) -> Result<String, i32> {
@@ -74,7 +158,7 @@ impl Wallet {
 }
 
 pub struct WalletManager {
-    wallets : Vec<Wallet>
+    pub wallets : Vec<Wallet>
 }
 
 impl WalletManager {
@@ -82,17 +166,16 @@ impl WalletManager {
         WalletManager { wallets : Vec::new() }
     }
 
-    pub fn open_wallet(&mut self, path : &String, password : &String) -> Result<&Wallet, i32> {
-        
-        let mut wallet = Wallet::open(path, password).unwrap();
-        
+    pub fn open_wallet(&mut self, path : String, password : &String) {
+        let wallet = Wallet::open(path, password).unwrap();
         self.wallets.push(wallet);
+    }
 
-        if let Some(r) = self.wallets.last() {
-            Ok( r )
-        }
-        else {
-            Err(0)
-        }
+    pub fn add_note(&mut self, wid : usize, id : u32, name : String, content : String) {
+        &self.wallets[wid].add_note(id, name, content);
+    }
+
+    pub fn show(&self, wid : usize) {
+        print!("{:?}\n", self.wallets[wid]);
     }
 }
